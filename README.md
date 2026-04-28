41a6d2525460a0ab5e190ee5008ae32d933c396b
Håvard Pedersen <fuzzy76@fuzzy76.net>
2026-04-28 21:18:51 +0200

    Rewrite to output NotePlan 3 compatible file structure

--- README.md
+++ README.md
@@ -1,11 +1,40 @@
 # things3export
-A script that connects to your local Things 3 sqlite database and dumps the contents in an XML structure. Meant more as a demonstration on how to parse the Things 3 database into something that makes sense.
+A script that connects to your local Things 3 sqlite database and dumps the contents in a structure of folders (one for each area) and markdown files (one for each project, one index for each area, and one index at top level) - modeled after NotePlan.
 
-WARNING: This code does not escape stuff very well. Please only use it on a copy of your Things database. And please verify the output before using it for anything important.
+WARNING: This code does not escape stuff very well. Please only use it on a copy of your personal Things database. It's probably possible to craft a malicious database that could write elsewhere on your system. And verify the output before using it for anything important.
 
-Requirements: Python 3
+Requirements: Python 3 (no dependencies)
 
 1. Find your database according to [this article](https://culturedcode.com/things/support/articles/2982272/) and **copy** it to the folder of this script.
-2. Run the script from terminal using `python3 things3export.py > output.xml`
-3. `output.xml` now contains your data!
+2. Run the script from terminal using `python3 things3export.py`
+3. The directory `export_noteplan3` now contains your data!
 
+Caveats:
+1. Slashes in projects names are replaced with " or ".
+2. Spaces in tags are replaced with _.
+3. Repeating is not supported at all, but marked with a tag.
+4. Reminders is not supported, but left out.
+5. Deadlines is not supported in NotePlan.
+6. NotePlan date: If a task is marked as today in Things 3, it will be in NotePlan. If not i a task has a start date, that will be the date of a task. If it doesn't, but has a deadline, that will be the date of the task.
+7. Project properties are not inherited down to tasks. So dates and tags are just dumped in the document.
+8. Someday/maybe tasks are created as checklist items and tagged #someday.
+9. Someday/maybe, cancellations and such are not supported on project level.
+10. Cancelled and completed projects are not included for now.
+11. Nested tags are flattened
+
+## TODO
+* Separate reading and writing, so multiple outputs can be supported
+* Handle nested tags
+* Dump cancelled or completed projects to an archive file instead of ignoring them
+
+## Known bugs
+* The starttime and endtime Front matter for projects is wrong. It looks like Things 3 stores this as something else than timestamps?
+
+## Documentation tidbits:
+
+### TMTask
+This is the important table, which keeps projects, headings and tasks.
+* Type: 0 for task, 1 for project, 2 for heading
+* Status: 0 for not done, 1 seems unused, 2 for cancelled, 3 for completed
+* Start: 0 for inbox, 1 for available, 2 for some day
+* todayIndexReferenceDate if exists, indicates today
--- things3export.py
+++ things3export.py
@@ -1,19 +1,24 @@
+# This is spaghetti-code Python. Start from the bottom :)
+
 # Imports
-import sqlite3, html
+import sqlite3, os, datetime
 
 # Global variables
 dbconn = None
+exportdir = None
 
-# 3 = completed, 2 = cancelled
-filtertasks = "trashed=0 AND status != 3 AND status != 2"
-
-# Columns we ignore for tasks - notes are handled separately, the rest are BLOBs
-ignorecols = ('notes', 'cachedTags', 'rt1_recurrenceRule', 'experimental', 'repeater')
+filtertasks = "trashed=0"
 
 # Helper for making sqlite return dictionary rows
 def dict_factory(cursor, row):
     fields = [column[0] for column in cursor.description]
     return {key: value for key, value in zip(fields, row)}
+
+# Convert Things 3 timestamp to NotePlan date string
+def timestamp_to_date(timestamp):
+    out = datetime.date.fromtimestamp(timestamp)
+    out = out.strftime("%Y-%m-%d %H:%M")
+    return out
 
 # Send a query to sqlite, and return all rows
 def query(querytext):
@@ -21,128 +26,202 @@ def query(querytext):
     dbcursor.execute(querytext)
     return dbcursor.fetchall()
 
-# Convert row values to a list of attributes for XML
-def get_attributelist(row):
-    attributes = ''
-    for key, value in row.items():
-        if not key in ignorecols:
-            if isinstance(value, str):
-                value = html.escape(value)
-            attributes = attributes + f"{key}=\'{value}\' "
-    return attributes.rstrip(' ')
+# Create a unique filename by adding a number until we find something that doesn't already exist.
+def uniquify(path):
+    filename, extension = os.path.splitext(path)
+    counter = 1
+
+    while os.path.exists(path):
+        path = filename + " (" + str(counter) + ")" + extension
+        counter += 1
+
+    return path
+
+# Output frontmatter
+def write_frontmatter(f, frontmatter):
+    if not frontmatter:
+        return
+    
+    f.write("---\n")
+    for key, value in frontmatter.items():
+        f.write(f"{key}: {value}\n")
+    f.write("---\n")
 
 # Get tags for a task ID
 def get_taglist(task):
     output = query(f"SELECT * FROM TMTaskTag INNER JOIN TMTag ON TMTaskTag.tags = TMTag.uuid WHERE TMTaskTag.tasks='{task['uuid']}'")
     tags = ''
     for row in output:
-        tags = tags + '"' + row['title'] + '",'
-    return tags.rstrip(',')
+        tags = tags + '#' + row['title'].replace(' ', '_') + ' '
+    return tags.rstrip(' ')
 
 # Get, and out, checklist items for a task
-def handle_checklist(task):
+def handle_checklist(task, f):
     output = query(f"SELECT * FROM TMChecklistItem WHERE task='{task['uuid']}' ORDER BY \"index\"")
     if len(output) == 0:
         return
-    print('<checklist>')
     for row in output:
-        attributes = get_attributelist(row)
-        print(f"<item {attributes} />")
-    print('</checklist>')
+        f.write(f"+ {row['title']}")
+
+# Output a task
+def handle_task(task, f):
+
+    taglist = get_taglist(task)
+
+    tasktype = "*"
+    if task['start'] == 0:
+        taglist = (taglist + " #inbox").lstrip(' ')
+    elif task['start'] == 2:
+        taglist = (taglist + " #someday").lstrip(' ')
+        tasktype = "-"
+    if task['status'] == 2:
+        tasktype = "- [-]"
+    elif task['status'] == 3:
+        taglist = (taglist + " @done("+timestamp_to_date(task['stopDate'])+")").lstrip(' ')
+        tasktype = "- [x]"
+
+    datestring = ""
+    if task['todayIndexReferenceDate'] is not None:
+        taglist = (taglist + " #today").lstrip(' ')
+        datestring=">today"
+    elif task['startDate'] is not None:
+        datestring = timestamp_to_date(task['startDate'])
+    elif task['deadline'] is not None:
+        datestring = timestamp_to_date(task['deadline'])
+    if task['rt1_recurrenceRule'] is not None:
+        taglist = (taglist + " #repeating_template").lstrip(' ')
+
+    f.write(f"{tasktype} {task['title']} {taglist} {datestring}\n")
+    if len(task['notes']) > 0:
+        f.write(f"{task['notes']}\n")
+    handle_checklist(task, f)
 
 # Output a heading
-def handle_heading(heading):
-    attributes = get_attributelist(heading)
-    tags = get_taglist(heading)
-    print(f"<heading {attributes} tags='{tags}'>")
-    # Headings don't have tags, but I don't want to assume anything
+def handle_heading(heading, f):
+    # Things 3 might not support tags for headings, but we do :)
+    taglist = get_taglist(heading)
+    f.write(f"## {heading['title']}\n")
+    f.write(f"{taglist}\n")
     if len(heading['notes']) > 0:
         # Headings don't have notes, but I don't want to assume anything
-        print('<note>' + html.escape(heading['notes']) + '</note>')
-    handle_checklist(heading)
+        f.write(f"{heading['notes']}\n")
+    handle_checklist(heading, f)
     output = query(f"SELECT * FROM TMTask WHERE {filtertasks} AND heading='{heading['uuid']}' ORDER BY \"index\", \"type\"")
     for row in output:
         # Headings only contains tasks for now, but I don't want to assume anything
         if row['type'] == 0:
-            handle_task(row)
+            handle_task(row, f)
         if row['type'] == 1:
-            handle_project(row)
+            print("Projects under a heading is not supported - ignoring.", row)
         if row['type'] == 2:
-            handle_heading(row)
-    print('</heading>')
+            # Heading under a heading? Weird...
+            handle_heading(row, f)
 
 # Output a project
 def handle_project(project):
-    attributes = get_attributelist(project)
-    tags = get_taglist(project)
-    print(f"<project {attributes} tags='{tags}'>")
-    if len(project['notes']) > 0:
-        print('<note>' + html.escape(project['notes']) + '</note>')
-    handle_checklist(project)
-    output = query(f"SELECT * FROM TMTask WHERE {filtertasks} AND project='{project['uuid']}' ORDER BY \"index\", \"type\"")
-    for row in output:
-        if row['type'] == 0:
-            handle_task(row)
-        if row['type'] == 1:
-            # Subprojects is not really a thing yet, but I don't want to assume anything
-            handle_project(row)
-        if row['type'] == 2:
-            handle_heading(row)
-    print('</project>')
-
-# Output a task
-def handle_task(task):
-    attributes = get_attributelist(task)
-    tags = get_taglist(task)
-    print(f"<task {attributes} tags='{tags}'>")
-    if len(task['notes']) > 0:
-        print('<note>' + html.escape(task['notes']) + '</note>')
-    handle_checklist(task)
-    print('</task>')
+    # We do not output finished or cancelled projects
+    if project['status'] in (2, 3):
+        return
+    taglist = get_taglist(project)
+    filename = f"{project['title'].replace("/", " or ")}.md"
+    # We might have several projects with the same name
+    filename = uniquify(filename)
+    with open(filename, "w") as f:
+        frontmatter = {
+            "Title" : project['title'],
+            "Created" : timestamp_to_date(project['creationDate'])
+        }
+        if project['startDate'] is not None:
+            frontmatter['Startdate'] = timestamp_to_date(project['startDate'])
+        if project['deadline'] is not None:
+            frontmatter['Deadline'] = timestamp_to_date(project['deadline'])
+        if project['rt1_recurrenceRule'] is not None:
+            taglist = (taglist + " #repeating_template").lstrip(' ')
+        if project['todayIndexReferenceDate'] is not None:
+            taglist = (taglist + " #today").lstrip(' ')
+        if project['start'] == 2:
+            taglist = (taglist + " #someday").lstrip(' ')
+        if project['status'] == 2:
+            taglist = (taglist + " #cancelled").lstrip(' ')
+        if project['status'] == 3:
+            taglist = (taglist + " #completed").lstrip(' ')
+        write_frontmatter(f, frontmatter)
+        f.write(f"# {project['title']}\n")
+        f.write(f"{taglist}\n")
+        if len(project['notes']) > 0:
+            f.write(project['notes'] + '\n')
+        handle_checklist(project, f)
+        output = query(f"SELECT * FROM TMTask WHERE {filtertasks} AND project='{project['uuid']}' ORDER BY \"index\", \"type\"")
+        for row in output:
+            if row['type'] == 0:
+                handle_task(row, f)
+            if row['type'] == 1:
+                print("Subprojects are not supported - ignoring.", row)
+            if row['type'] == 2:
+                handle_heading(row, f)
 
 # Output an area
 def handle_area(area):
-    attributes = get_attributelist(area)
-    print(f"<area {attributes}>")
-    output = query(f"SELECT * FROM TMTask WHERE {filtertasks} AND area='{area['uuid']}' ORDER BY \"index\", \"type\"")
+    os.mkdir(area['title'])
+    os.chdir(area['title'])
+
+    output = query(f"SELECT * FROM TMTaskTag INNER JOIN TMTag ON TMTaskTag.tags = TMTag.uuid WHERE TMTaskTag.tasks='{area['uuid']}'")
+    taglist = ''
     for row in output:
-        if row['type'] == 0:
-            handle_task(row)
+        taglist = taglist + '#' + row['title'].replace(' ', '_') + ' '
+    taglist = taglist.rstrip(' ')
+
+    # Handle items in area
+    output = query(f"SELECT * FROM TMTask WHERE {filtertasks} AND area='{area['uuid']}' ORDER BY \"index\", \"type\"")
+
+    with open ("index.md", "w") as f:
+        # Only way I can think of tagging an area in NotePlan
+        f.write(f"{taglist}\n")       
+        for row in output:
+            if row['type'] == 0:
+                handle_task(row, f)
+            if row['type'] == 2:
+                # Areas don't really support headings yet, but the data structure does, so let's pretend
+                handle_heading(row, f)
+
+    for row in output:
         if row['type'] == 1:
             handle_project(row)
-        if row['type'] == 2:
-            # Areas don't support headings yet, but I've asked Cultured Code for it :)
-            handle_heading(row)
-    print('</area>')
+    os.chdir('..')
 
 # Output the Things3 contents
 def handle_things():
-    print('<things3>')
 
     # Handle items without area
     output = query(f"SELECT * FROM TMTask WHERE {filtertasks} AND area IS NULL AND project IS NULL AND heading IS NULL ORDER BY \"index\", \"type\"")
+
+    with open ("index.md", "w") as f:
+        for row in output:
+            if row['type'] == 0:
+                handle_task(row, f)
+            if row['type'] == 2:
+                print("Heading without area - ignoring.", row)
+
     for row in output:
-        if row['type'] == 0:
-            handle_task(row)
         if row['type'] == 1:
             handle_project(row)
-        if row['type'] == 2:
-            # A heading with nowhere to go makes no sense, but I don't want to assume anything.
-            handle_heading(row)
 
     # Handle areas
     output = query('SELECT * FROM TMArea ORDER BY "index"')
     for row in output:
         handle_area(row)
 
-    print('</things3>')
-
 # Set up database
 database = "main.sqlite"
 dbconn = sqlite3.connect(database)
 dbconn.row_factory = dict_factory
 
+# Set up output directory
+os.makedirs("export_noteplan3", exist_ok=True)
+os.chdir("export_noteplan3")
+
 handle_things()
 
 # Close up
+os.chdir('..')
 dbconn.close()
